28c973a5cda7bfd14d2b1879e18491066d97694a
Flight-of-Dragons <jeremy.barrass@gmail.com>
2016-04-26 12:29:01 +0100

    Adding src/<class> file to test directory leads to passing tests. Possibly need to work out how to import or direct tests to src files properly.

--- auto-character-sheet/test/TestCharacterSheet.java
+++ auto-character-sheet/test/TestCharacterSheet.java
@@ -6,8 +6,19 @@ public class TestCharacterSheet {
 	CharacterSheet manny = new CharacterSheet(name);
 
 	@Test
-	public void testCharacterName() {
-		System.out.print("Character's name stored: ");
-		assertEquals(manny.characterName, name);
+	public void testGetCharacterName() {
+		System.out.println("Character's name stored: ");
+		assertEquals(manny.getCharacterName(), name);
 	}
+
+	@Test
+	public void testSetCharacterName() {
+		String newName = "Jonny";
+		manny.setCharacterName(newName);
+		System.out.println("Character's name changed: ");
+		assertEquals(manny.getCharacterName(), newName);
+	}
+
+	@Test
+	public void test
 }
